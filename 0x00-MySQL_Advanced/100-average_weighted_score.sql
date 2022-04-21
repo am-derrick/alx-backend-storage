93fe7d3ff742b438ef9517dc2dcd8e9e5fabdeb9
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-22 00:07:36 +0300

    DELIMITER & function typo corrected

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,9 +1,9 @@
 -- creates a stores procedure ComputeAverageWeightedScoreForUser
 -- that computes and store the average weighted score for a student
 
-DELIMETER $$
+DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-CREATE PROCEDURE ComputeAverageWeightesScoreForUser (IN `user_id` INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN `user_id` INT)
 BEGIN
 	UPDATE users
 	SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
@@ -13,4 +13,4 @@ BEGIN
 	    WHERE corrections.user_id = user_id)
 	WHERE users.id = user_id;
 END $$
-DELIMETER ;$$
+DELIMITER ;$$
